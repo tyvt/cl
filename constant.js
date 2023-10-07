e4540ccaddc58f8bc42389d51b370ff128cfb4c8
javascriptfield <15190562801@163.com>
2023-10-07 11:54:49 +0800

    init

---
+++ constant.js
@@ -0,0 +1,11 @@
+export const CL_DOMAIN = 'https://cl.3572y.xyz'
+export const RM_DOWN_DOMAIN = 'http://www.rmdown.com/link.php'
+export const HEADERS = {
+  authority: 'cl.3572y.xyz',
+  pragma: 'no-cache',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+  'User-Agent': 'Apifox/1.0.0 (https://apifox.com)'
+}
+
+export const MAGNET_PREFIX = 'magnet:?xt=urn:btih:'
---
+++ url_magnet.js
@@ -0,0 +1,52 @@
+import axios from "axios"
+import { load } from "cheerio"
+import fs from "fs"
+import {
+  HEADERS,
+  MAGNET_PREFIX,
+  RM_DOWN_DOMAIN,
+} from "./constant.js"
+import { sleep } from "./utils.js"
+const SLEEP_TIME = 3000
+const data = fs.readFileSync("./url.txt", "utf-8")
+const urls = data.split('\n').filter(e => e)
+const getRMdownUrl = (url) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      url: url,
+      headers: HEADERS,
+    }).then((res) => {
+      const $ = load(res.data)
+      const linkTags = $('[id=conttpc]').find(`a:contains('${RM_DOWN_DOMAIN}')`)
+      for (const iterator of linkTags) {
+        resolve(iterator.attribs.href)
+      }
+    })
+  })
+}
+const getMagnetUrl = (url) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      url: url,
+      headers: HEADERS,
+    }).then((res) => {
+      const hashCode = res.data.match(
+        /(?<=Code:\s)(.*?)(?=<\/span>)/g
+      )[0]
+      console.log('magnet', `${MAGNET_PREFIX}${hashCode}`)
+      resolve(`${MAGNET_PREFIX}${hashCode}`)
+    })
+  })
+}
+const start = async () => {
+  for (let index = 0; index < urls.length; index++) {
+    const RMDownUrl = await getRMdownUrl(urls[index])
+    const magnetUrl = await getMagnetUrl(RMDownUrl)
+    const data = fs.readFileSync("./magnet.txt", "utf-8")
+    fs.writeFileSync("./magnet.txt", `${data}\n${magnetUrl}`)
+    sleep(SLEEP_TIME)
+  }
+}
+start()
