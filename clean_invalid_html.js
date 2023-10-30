d2454c495275525ad1e234e3c499530bc67f74ed
javascriptfield <15190562801@163.com>
2023-10-30 13:05:39 +0800

    remove black html

---
+++ clean_invalid_html.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+const dir = `./pages/16/`;
+const list = fs.readdirSync(dir);
+const blackList = fs
+  .readFileSync(`./urls/blackList.txt`, "utf-8")
+  .split("\n")
+  .filter((e) => e);
+list.forEach((e) => {
+  const html = fs.readFileSync(`${dir}${e}`, "utf-8");
+  if (blackList.some((url) => html.includes(url))) {
+    fs.unlinkSync(`${dir}${e}`);
+  }
+});
