e1bbe4a9e267204885f889cf2e2bdd693b308443
javascriptfield <15190562801@163.com>
2024-02-07 11:27:29 +0800

    update script

--- utils.js
+++ utils.js
@@ -32,7 +32,7 @@ export class DBHelper {
     let sql = `INSERT OR IGNORE INTO ${tableName} (${Object.keys(
       arr[0]
     ).join()}) VALUES `
-    const sqlArr = arr.slice(0, 2).map((e) => {
+    const sqlArr = arr.map((e) => {
       return `(${Object.values(e).join()})`
     })
     sql = sql.concat(`${sqlArr.join(",")}`)
