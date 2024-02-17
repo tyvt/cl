4fa32e8eaf7f8a0a1cc5a349e5d25fb6e44ebdfc
example <example@example.com>
2024-02-17 15:44:54 +0800

    update script

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 import { get, DBHelper } from "./utils.js"
 const DB = new DBHelper()
-DB.update(
-  `t_topic`,
-  {
-    name: '1',
-  },
-  `url = "/2310/2/5994922"`
-).then(async (res) => {
-  console.log('res: ', res)
+DB.runSQL('SELECT * FROM t_content tc WHERE tc.content LIKE "%class=%"').then(async res => {
+  for await (const iterator of res[0].values) {
+    await DB.update('t_content', {
+      content: iterator[2].replace(/class='.*?'/g, '')
+    }, `url = "${iterator[1]}"`)
+  }
 })
