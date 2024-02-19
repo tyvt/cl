fe00a768f0514a37956051f53f669dc24fa4f85e
example <example@example.com>
2024-02-19 09:32:36 +0800

    update script

--- crawler_detail_page_content.js
+++ crawler_detail_page_content.js
@@ -24,8 +24,11 @@ const start = async () => {
         sleep(2500)
         continue
       }
-      const post_time = data.match(/(?<=data-timestamp=").*?(?=")/)[0]
-      await DB.update('t_topic', { 'post_time': post_time }, `url = "${iterator[2]}"`)
+      const matchedTime = data.match(/(?<=data-timestamp=").*?(?=")/)
+      const post_time = matchedTime?.[0]
+      if (post_time) {
+        await DB.update('t_topic', { 'post_time': post_time }, `url = "${iterator[2]}"`)
+      }
       const html = `${matched[0]
         .replace(/ess-data/g, "src")
         .replace(/&nbsp;/g, "")
