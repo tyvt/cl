9d327ba414001c4e8d89f05c8f07db5072905b51
example <example@example.com>
2024-03-04 14:24:42 +0800

    publish change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-// https://vitejs.dev/config/
-let version
-exec('npm pkg get version', (_, stdout) => {
-  version = stdout.replace(/\n/, '').replace(/"/g, '')
-})
-export default defineConfig({
-  base: `https://unpkg.com/cl-lite@${version}`,
-  plugins: [
-    uni.default(),
-  ],
+import { exec } from 'child_process'
+
+export default defineConfig(async ({ command, mode, ssrBuild }) => {
+  let version
+  exec('npm pkg get version', (_, stdout) => {
+    version = stdout.replace(/\n/, '').replace(/"/g, '')
+  })
+  return {
+    base: `https://unpkg.com/cl-lite@${version}`,
+    plugins: [
+      uni.default(),
+    ],
+  }
+
 })
