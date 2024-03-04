e235af2470243ef0cb142e495b05b57d69001781
example <example@example.com>
2024-03-04 14:21:38 +0800

    publish change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
+let version
+exec('npm pkg get version', (_, stdout) => {
+  version = stdout.replace(/\n/, '').replace(/"/g, '')
+})
 export default defineConfig({
+  base: `https://unpkg.com/cl-lite@${version}`,
   plugins: [
     uni.default(),
   ],
