0654d25290c46971bce3f8e4fc2e880dbe6a5ae7
example <example@example.com>
2024-03-28 19:16:39 +0800

    update db

--- jobs/test.js
+++ jobs/test.js
@@ -1,5 +1,5 @@
 import { get, DBHelper } from "./utils.js"
-const DB = new DBHelper("./db/cl-detail-2.sqlite")
+const DB = new DBHelper("./db/cl-detail-15.sqlite")
 
 function removeClass() {
   DB.runSQL('SELECT * FROM t_content tc WHERE tc.content LIKE "%class=%"').then(async res => {
@@ -16,7 +16,8 @@ function removeClass() {
 
 function removeStyle() {
   DB.runSQL('SELECT * FROM t_content tc WHERE tc.content LIKE "%style=%"').then(async res => {
-    for await (const iterator of res[0].values) {
+    for await (let [index, iterator] of res[0].values.entries()) {
+      console.log(`${index}/${res[0].values.length}`)
       await DB.update('t_content', {
         content: iterator[1].replace(/style='.*?'/g, '')
       }, `url = "${iterator[0]}"`)
@@ -25,3 +26,4 @@ function removeStyle() {
 }
 
 removeClass()
+// removeStyle()
