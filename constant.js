5cacee91ad11d5ada2cb058c055c633ea614653e
example <example@example.com>
2024-03-28 08:50:29 +0800

    update domain

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
-export const CL_DOMAIN = "https://cl.6235x.xyz"
+export const CL_DOMAIN = "https://www.t66y.com"
 export const DETAIL_PAGE_PREFIX = "htm_data"
 export const RM_DOWN_DOMAIN = "http://www.rmdown.com/link.php"
 export const MAGNET_PREFIX = "magnet:?xt=urn:btih:"
