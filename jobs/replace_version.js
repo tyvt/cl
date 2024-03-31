4f3f28a3bd07eacbdc8d9a1d30114dad4e60c1de
example <example@example.com>
2024-03-31 17:03:15 +0800

    update version

--- jobs/replace_version.js
+++ jobs/replace_version.js
@@ -4,5 +4,5 @@ exec('npm pkg get version', (_, stdout) => {
   const version = stdout.replace(/\n/, '').replace(/"/g, '')
   const html = fs.readFileSync('./index.html', 'utf-8').replace(/(?<=<title>).*?(?=<\/title>)/, version)
   fs.writeFileSync('./index.html', html)
-  fs.writeFileSync('src/static/version.js', `export default ${version}`)
+  fs.writeFileSync('src/static/version.js', `export default "${version}"`)
 })
