730440c745cdbfdebe52c26ba6d7fd6c4939d2e2
example <example@example.com>
2024-04-02 17:41:12 +0800

    update db

--- jobs/remove_class_style.js
+++ jobs/remove_class_style.js
@@ -1,5 +1,5 @@
 import { DBHelper } from "./utils.js"
-const DB = new DBHelper("./db/cl-detail-5.sqlite")
+const DB = new DBHelper("./db/cl-detail-15.sqlite")
 
 async function removeClass() {
   await DB.runSQL('SELECT * FROM t_content tc WHERE tc.content LIKE "%class=%"').then(async res => {
@@ -25,5 +25,32 @@ async function removeStyle() {
   })
 }
 
+async function removeScript() {
+  await DB.runSQL('SELECT * from t_content tc WHERE tc.content LIKE "%<script%"').then(async res => {
+
+    for await (let [index, iterator] of res[0].values.entries()) {
+      console.log(`${index}/${res[0].values.length}`)
+
+      await DB.update('t_content', {
+        content: iterator[1].replace(/<script.*?>.*?<\/script>/g, '')
+      }, `url = "${iterator[0]}"`)
+    }
+  })
+}
+
+async function removeLink() {
+  await DB.runSQL('SELECT * from t_content tc WHERE tc.content LIKE "%<link%"').then(async res => {
+
+    for await (let [index, iterator] of res[0].values.entries()) {
+      console.log(`${index}/${res[0].values.length}`)
+
+      await DB.update('t_content', {
+        content: iterator[1].replace(/<link.*?>/g, '')
+      }, `url = "${iterator[0]}"`)
+    }
+  })
+}
 await removeClass()
 await removeStyle()
+await removeScript()
+await removeLink()
