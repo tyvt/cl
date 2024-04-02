a7e30836506bbc25a12a1653a44d7e5649566a02
example <example@example.com>
2024-04-02 10:35:44 +0800

    update db

--- jobs/removeText.js
+++ jobs/removeText.js
@@ -3,10 +3,10 @@ const DB_DETAIL_7 = new DBHelper("./db/cl-detail-7.sqlite")
 const DB_CATEGORY_7 = new DBHelper("./db/cl-category-7.sqlite")
 
 
-DB_DETAIL_7.runSQL('SELECT url from t_content tc WHERE tc.content NOT LIKE "%<img%"').then(async res => {
+await DB_DETAIL_7.runSQL('SELECT url from t_content tc WHERE tc.content NOT LIKE "%<img%"').then(async res => {
   for await (let [index, iterator] of res[0].values.entries()) {
     console.log(`${index}/${res[0].values.length}`)
-
+    await DB_DETAIL_7.runSQL(`delete from t_content where url = "${iterator[0]}"`)
     await DB_CATEGORY_7.runSQL(`delete from t_topic where url = "${iterator[0]}"`)
   }
 })
