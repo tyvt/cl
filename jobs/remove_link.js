8c4e346d71d72acbe7f9f6559010eed9f25e2ede
ty <example@example.com>
2024-04-18 08:43:38 +0800

    update db

--- jobs/remove_link.js
+++ jobs/remove_link.js
@@ -1,12 +1,12 @@
 import { DBHelper } from "./utils.js"
 const DB_DETAIL_2 = new DBHelper("./db/cl-detail-15.sqlite")
-const str = "发牌"
+const str = "凤凰娱乐"
 await DB_DETAIL_2.runSQL(`SELECT * from t_content tc WHERE content like "%${str}%"`).then(async res => {
   if (res.length !== 0) {
     for await (let [index, iterator] of res[0].values.entries()) {
       console.log(`${index + 1}/${res[0].values.length}`)
       await DB_DETAIL_2.update('t_content', {
-        content: iterator[1].replace(/<span.*?(?=【下)/, '')
+        content: iterator[1].replace(/<span.*?(?=【影片)/, '')
       }, `url = "${iterator[0]}"`)
     }
   }
