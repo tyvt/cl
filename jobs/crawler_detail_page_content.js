0907bb422b140a78ced5e98e625fa0e509c574b7
ty <example@example.com>
2024-05-22 16:26:53 +0800

    update job

--- jobs/crawler_detail_page_content.js
+++ jobs/crawler_detail_page_content.js
@@ -7,51 +7,51 @@ const detail = async (fid) => {
     `SELECT url FROM t_topic tt WHERE tt.post_time = '' OR tt.post_time ISNULL ORDER BY rowid DESC`
   ).then(async (res) => {
     const list = res?.[0]?.values || []
-    for await (const iterator of list) {
-      const url = `${CL_DOMAIN}/${DETAIL_PAGE_PREFIX}${iterator[0]}.html`
-      const { data } = await get(url)
-      // copyToClipboard(data);
-      if (data.includes(`無法找到頁面`)) {
-        await DB_CATEGORY.runSQL(`delete from t_topic where url = "${iterator[0]}"`)
-        sleep(2200)
-        continue
-      }
-      const matched = data.match(
-        /<div\sclass="tpc_content do_not_catch"\sid="conttpc">.*/
-      )
-      if (!matched) {
-        sleep(2200)
-        continue
-      }
-      let post_time
-      let matchedTime = data.match(/(?<=data-timestamp=").*?(?=")/)
-      if (matchedTime?.[0]) {
-        post_time = matchedTime?.[0]
-      } else {
-        matchedTime = data.match(/Posted:.*/)
-        const matchDate = matchedTime?.[0].replace('Posted:', '')
-        post_time = new Date(matchDate).valueOf() / 1000
-      }
-      console.log('post_time: ', post_time)
-      if (post_time) {
-        await DB_CATEGORY.update('t_topic', { 'post_time': post_time }, `url = "${iterator[0]}"`)
-      }
-      const html = `${matched[0]
-        .replace(/ess-data/g, "src")
-        .replace(/&nbsp;/g, "")
-        .replace(/\siyl-data='http:\/\/a.d\/adblo_ck.jpg'/g, "")
-        .replace(/\sdata-link='.*?'/g, "")
-        .replace(/<script.*?>.*?<\/script>/g, "")
-        .replace(/<link.*?>/g, "")
-        .replaceAll(`class="tpc_content do_not_catch" id="conttpc"`, "").replace(/\"/g, "'").replace(/\s+/g, ' ')}
-          </div><br>`
-      const DB_DETAIL = new DBHelper(`./db/cl-detail-${fid}.sqlite`)
-      await DB_DETAIL.insert("t_content", [{
-        url: iterator[0],
-        content: html,
-      }])
-      sleep(2200)
-    }
+    // for await (const iterator of list) {
+    //   const url = `${CL_DOMAIN}/${DETAIL_PAGE_PREFIX}${iterator[0]}.html`
+    //   const { data } = await get(url)
+    //   // copyToClipboard(data);
+    //   if (data.includes(`無法找到頁面`)) {
+    //     await DB_CATEGORY.runSQL(`delete from t_topic where url = "${iterator[0]}"`)
+    //     sleep(2200)
+    //     continue
+    //   }
+    //   const matched = data.match(
+    //     /<div\sclass="tpc_content do_not_catch"\sid="conttpc">.*/
+    //   )
+    //   if (!matched) {
+    //     sleep(2200)
+    //     continue
+    //   }
+    //   let post_time
+    //   let matchedTime = data.match(/(?<=data-timestamp=").*?(?=")/)
+    //   if (matchedTime?.[0]) {
+    //     post_time = matchedTime?.[0]
+    //   } else {
+    //     matchedTime = data.match(/Posted:.*/)
+    //     const matchDate = matchedTime?.[0].replace('Posted:', '')
+    //     post_time = new Date(matchDate).valueOf() / 1000
+    //   }
+    //   console.log('post_time: ', post_time)
+    //   if (post_time) {
+    //     await DB_CATEGORY.update('t_topic', { 'post_time': post_time }, `url = "${iterator[0]}"`)
+    //   }
+    //   const html = `${matched[0]
+    //     .replace(/ess-data/g, "src")
+    //     .replace(/&nbsp;/g, "")
+    //     .replace(/\siyl-data='http:\/\/a.d\/adblo_ck.jpg'/g, "")
+    //     .replace(/\sdata-link='.*?'/g, "")
+    //     .replace(/<script.*?>.*?<\/script>/g, "")
+    //     .replace(/<link.*?>/g, "")
+    //     .replaceAll(`class="tpc_content do_not_catch" id="conttpc"`, "").replace(/\"/g, "'").replace(/\s+/g, ' ')}
+    //       </div><br>`
+    //   const DB_DETAIL = new DBHelper(`./db/cl-detail-${fid}.sqlite`)
+    //   await DB_DETAIL.insert("t_content", [{
+    //     url: iterator[0],
+    //     content: html,
+    //   }])
+    //   sleep(2200)
+    // }
     await count(fid)
   })
 }
@@ -62,7 +62,7 @@ const count = async (fid) => {
   const DB_DETAIL = new DBHelper(`./db/cl-detail-${fid}.sqlite`)
   if(size > 90000000) {
     const DB_CATEGORY = new DBHelper(`./db/cl-category-${fid}.sqlite`)
-    await DB_CATEGORY.runSQL(`SELECT url FROM t_topic ORDER BY post_time ASC LIMIT 100`).then(async categoryRes => {
+    await DB_CATEGORY.runSQL(`SELECT url FROM t_topic ORDER BY post_time ASC LIMIT 1000`).then(async categoryRes => {
       const list = categoryRes[0].values
       if(categoryRes[0].values) {
         for await (const iterator of list) {
@@ -94,10 +94,10 @@ async function main() {
       console.log(`Fetch ${category[0]} begin.`)
       await detail(category[1])
       const { size } = fs.statSync(`./db/cl-category-${category[1]}.sqlite`)
-      await DB.update('t_channel', {
-        update_time: Math.round(new Date().getTime() / 1000),
-        category_size: size
-      }, `fid = "${category[1]}"`)
+      // await DB.update('t_channel', {
+      //   update_time: Math.round(new Date().getTime() / 1000),
+      //   category_size: size
+      // }, `fid = "${category[1]}"`)
       console.log(`Fetch ${category[0]} end.`)
     }
   })
